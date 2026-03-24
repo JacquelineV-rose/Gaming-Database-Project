057814853e14b8e8ab3d8456bd1d6d1704f6ec5b
JacquelineV-rose <jvermette1010@gmail.com>
2026-03-24 17:37:43 -0400

    Setting up MySQL

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# Gaming is the database password
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:Gaming@localhost/group_project_db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+db = SQLAlchemy(app)
 
-@app.get("/")
+@app.route("/")
 def home():
-    return "Hello, Flask!"
-
+    return "API running!"
 
 if __name__ == "__main__":
     app.run(debug=True)
