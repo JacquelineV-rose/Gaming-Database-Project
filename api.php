800339fc832ee3b8df6d1ed579e8bad4f66f6352
annn-k <annnk2023@gmail.com>
2026-04-14 23:28:20 -0400

    implemented join functions

--- api.php
+++ api.php
@@ -3,9 +3,12 @@
 
     // header("Content-Type: application/json; charset=utf-8");
 
-    $query = "SELECT title, year, platform, age_rec FROM videogames";
+    //$query = "SELECT title, year, platform, age_rec FROM videogames";
 
-    // get column names for a table, return as an array
+    /* ============= INSERT, DELETE, UPDATE ============= */
+    // * Note: do not allow users to manually input the condition as a raw string. Probably best to use dropdowns to build condition.
+
+    // helper for insert/update -- get column names for a table, return as an array
     function get_columns($conn, string $table): array {
         $stmt = $conn->prepare("DESCRIBE $table");
         $stmt->execute();
@@ -24,7 +27,7 @@
         // builds a string like this --> "?, ?, ..." (? will be replaced by values in arg)
         $values_str = implode(", ", array_fill(0, count($columns), "?"));
 
-        $query = "INSERT INTO $table ($column_str) VALUES ($values_str)"
+        $query = "INSERT INTO $table ($column_str) VALUES ($values_str)";
         try {
             $stmt = $conn->prepare($query);
             $stmt->execute($values); // make sure values line up with the appropriate columns!!
@@ -35,6 +38,7 @@
     }
 
     // delete record in a table given conditions
+    // example usage: delete_record($conn, "videogames", "platform = 'Nintendo Switch' AND NOT company = 'Nintendo'");
     function delete_record($conn, string $table, string $condition) {
         $query = "DELETE FROM $table WHERE $condition";
 
@@ -66,12 +70,63 @@
         }
     }
 
-    // join multiple tables
+    /* ============= JOINING MULTIPLE TABLES ============= */
+    
+    // fetch all games produced by a given company
+    function all_company_games($conn, string $companyname) {
+        $query = "SELECT * FROM videogames JOIN company ON videogames.company = company.name WHERE company.name = ?"; // fetch entire tuple
+        $rows = [];
 
-    /* AGGREGATE QUERIES */
+        try {
+            $stmt = $conn->prepare($query);
+            $stmt->execute([$companyname]);
+            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            echo "Fetched games developed by " . $companyname;
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+
+        return $rows;
+    }
+
+    // fetch all reviews under a given game
+    function all_game_reviews($conn, string $gametitle) {
+        $query = "SELECT * FROM review JOIN videogames ON review.gametitle = videogames.title WHERE videogames.title = ?";
+        $rows = [];
+
+        try {
+            $stmt = $conn->prepare($query);
+            $stmt->execute([$gametitle]);
+            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            echo "Fetched reviews for " . $gametitle;
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+
+        return $rows;
+    }
+
+    // fetch all reviews left by a given user
+    function all_user_reviews($conn, string $username) {
+        $query = "SELECT * FROM review JOIN users ON review.username = users.username WHERE users.username = ?";
+        $rows = [];
+
+        try {
+            $stmt = $conn->prepare($query);
+            $stmt->execute([$username]);
+            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            echo "Fetched reviews by " . $username;
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+
+        return $rows;
+    }
+
+    /* ============= AGGREGATE QUERIES ============= */
 
     // total number of game ratings for a given game
-    function num_game_ratings($conn, $gameName) {
+    function num_game_ratings($conn, string $gameName) {
         //$query = "SELECT COUNT(*) FROM review WHERE gamename = $gameName";
         $query = "SELECT COUNT(*) FROM review WHERE gamename = ?";
         $rows = [];
@@ -87,7 +142,7 @@
     }
 
     // average rating for a given game
-    function average_rating($conn, $gameName) {
+    function average_rating($conn, string $gameName) {
         $query = "SELECT AVG(rating) FROM review WHERE gamename = ?";
         $rows = [];
         
@@ -103,7 +158,7 @@
     }
 
     // total number of games owned by a given user
-    function num_games_owned($conn, $username) {
+    function num_games_owned($conn, string $username) {
         $query = "SELECT COUNT(*) FROM plays WHERE username = ?";
         $rows = [];
         
@@ -118,11 +173,12 @@
         return $rows;
     }
 
+
     // stuff from original example
     try {
-        $stmt = $conn->query($query);
-        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        // // print_r($rows);
+        // $stmt = $conn->query($query);
+        // $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        // print_r($rows);
         // echo $row
 
         echo json_encode([
